c886afb00624eea56f6ff62a8fca6852a4362fca
Alex Percont <alexpercont@gmail.com>
2020-04-23 19:42:50 -0500

    Placeholder for first unit test.

---
+++ src/test/java/mx/com/percont/erp/services/products/InvalidProductExceptionMapperTest.java
@@ -0,0 +1,12 @@
+package mx.com.percont.erp.services.products;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvalidProductExceptionMapperTest {
+
+    @Test
+    public void invalidProductExceptionTest(){
+        Assert.assertTrue(true);
+    }
+}
